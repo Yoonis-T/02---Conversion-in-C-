f1a15993c011dd6dff8cb8afa0d0cd9da8ede69c
Yoonis-T <yoonis.abdulla@edu.prakticum.fi>
2025-10-28 15:27:03 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+    internal class Program
+    {
+        static void Main()
+        {
+        // 1. Definera en string som inehåller ett numer som text
+        string numberAsString = "123";
+
+        // 2. Konvertera strängen till ett heltal (int)
+        int number = int.Parse(numberAsString);
+
+        // 3. Skriv ut heltalet till konsolen
+        Console.WriteLine(number);
+    }
+}
+
